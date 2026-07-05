677203fb03a5d2a6f1f7f79e66aa032b76595482
Rafael Pransa <44833804+RafaelPransa@users.noreply.github.com>
2026-07-05 22:26:11 +0700

    chore: clean up unused temporary diagnostic and patch scripts from root folder

--- check-constraint.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const db = require('./src/config/db');
-db.raw("SELECT pg_get_constraintdef(oid) FROM pg_constraint WHERE conname = 'travel_bookings_booking_status_check'")
-  .then(res => {
-    fs.writeFileSync('constraint.txt', JSON.stringify(res.rows[0]));
-    process.exit(0);
-  });
